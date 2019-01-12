0ec4d356ff2dcd328753994e80a5a2a90dc4c9d1
navneetkumar-kiwi <35060141+navneetkumar-kiwi@users.noreply.github.com>
2019-01-12 13:37:15 +0530

    Push

--- src/main/java/pageObjectClasses/Property_Detail_POMs.java
+++ src/main/java/pageObjectClasses/Property_Detail_POMs.java
@@ -29,6 +29,44 @@ public class Property_Detail_POMs
 	@FindBy(xpath= "//span[@class = 'jss297']")
 	public WebElement detailaddress;
 	
+	@FindBy(xpath = "//div[@class='jss295']/span")
+	public List <WebElement> detaillogo;
+	
+	@FindBy(xpath = "//button[@aria-label='Close']")
+	public WebElement detailclose;
+	
+	@FindBy(xpath = "//div[@class = 'jss285']/div/h5")
+	public List <WebElement> detailbd;
+	
+	@FindBy(xpath= "//input[@placeholder = 'Name']")
+	public WebElement reqinfoname;
+	
+	@FindBy(xpath= "//input[@placeholder = 'Phone Number']")
+	public WebElement reqinfophone;
+	
+	@FindBy(xpath= "//input[@placeholder = 'Email']")
+	public WebElement reqinfoemail;
+	
+	@FindBy(xpath= "//textarea[@name = 'message']")
+	public WebElement reqinfomessage;
+		
+	@FindBy(xpath= "//span[contains(text(), 'Request Info')]")
+	public WebElement reqinfosubmit;
+	
+	@FindBy(xpath= "//div[@class = 'jss5 jss44']/h5")
+	public WebElement similarlabel;
+	
+	@FindBy(xpath= "//div[@class = 'slick-list']")
+	public WebElement images;
+	
+	@FindBy(xpath= "//div[@class = 'slick-track']/div")
+	public List <WebElement> images1;
+	
+	@FindBy(xpath= "//div[@class = 'jss4']")
+	public List <WebElement> similar;
+	
+	
+	
 	/***ACTIONS***/
 
 }
--- src/test/java/testClasses/Property_Detail_Test.java
+++ src/test/java/testClasses/Property_Detail_Test.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 import baseClass.DriverClass;
 import pageObjectClasses.Home_POMs;
 import pageObjectClasses.Property_Detail_POMs;
+import utlilty.WaitClass;
 
 public class Property_Detail_Test 
 
@@ -28,43 +29,23 @@ public class Property_Detail_Test
 		driver.get("https://uat.dealvestors.com/");Thread.sleep(2000);
 	}
 	
-	@Test(enabled=true)
-	public void a_ElementsCheck() throws InterruptedException
+	@Test(enabled=false)
+	public void a_basicdetails() throws InterruptedException
 	{
 		Thread.sleep(3000);
-		String price = ob6.tilegridprice.get(4).getText();
-		System.out.println(price);
-		
-		String address = ob6.tilegridaddress.get(4).getText();
-		System.out.println(address);
-		
-		String bd = ob6.tilegridbd.get(4).getText();
-		System.out.println(bd);
-		
+		String price = ob6.tilegridprice.get(6).getText();
+		String address = ob6.tilegridaddress.get(6).getText();
+		String bd = ob6.tilegridbd.get(6).getText();
 		String[] s1 = bd.split("•");
-		System.out.println(Arrays.toString(s1));
-		System.out.println(s1[0]);
-		String[] s2 = s1[0].split(" SQFT");
-		System.out.println(s2[0]);
-			
-	    System.out.println(s1[1]);
+		String[] s2 = s1[0].split(" SQFT");			
 		String[] s3 = s1[1].split(" BD");
-        System.out.println(s3[0]);
-        
-        System.out.println(s1[2]);
-        String[] s4 = s1[2].split(" BA");
-        System.out.println(s4[0]);
-        
-        ob6.tilegridprice.get(4).click();
+        String[] s4 = s1[2].split(" BA");    
+        ob6.tilegridprice.get(6).click();WaitClass.waitForWebElement(2000);
         Assert.assertEquals(price, ob5.detailprice.getText());
-        
         Assert.assertEquals(s2[0], ob5.detailsqft.get(2).getText());
-        
-        Assert.assertEquals(s3[0], ob5.detailsqft.get(0).getText());
-        
-        Assert.assertEquals(s4[0], ob5.detailsqft.get(1).getText());
-        
-        if (ob5.detailaddress.getText().contains(ob6.tilegridaddress.get(4).getText()))
+        Assert.assertEquals(s3[0], ob5.detailsqft.get(0).getText());        
+        Assert.assertEquals(s4[0], ob5.detailsqft.get(1).getText());    
+        if (ob5.detailaddress.getText().contains(address))
         {
 	      Assert.assertEquals(true, true);
         }
@@ -72,10 +53,44 @@ public class Property_Detail_Test
         {
 	       Assert.assertEquals(false, true);
         }
-
-        
-        Thread.sleep(2000);
-		
+        WaitClass.waitForWebElement(2000);
+	}
+	
+	@Test(enabled=false)
+	public void b_ElementsCheck() throws InterruptedException
+	{
+		ob6.tile.click();Thread.sleep(2000);
+		Assert.assertEquals(ob5.detaillogo.get(0).isDisplayed() && ob5.detaillogo.get(0).getSize().getWidth() !=0 && ob5.detaillogo.get(0).getSize().getHeight() !=0, true);
+        Assert.assertEquals(ob5.detailaddress.isDisplayed(), true);
+        Assert.assertEquals(ob5.detailclose.isDisplayed() && ob5.detailclose.getSize().getHeight() !=0 && ob5.detailclose.getSize().getWidth() !=0, true);
+        Assert.assertEquals(ob5.detailprice.isDisplayed(), true);
+        Assert.assertEquals(ob5.detailbd.get(0).getText(), "Beds");
+        Assert.assertEquals(ob5.detailbd.get(1).getText(), "Baths");
+        Assert.assertEquals(ob5.detailbd.get(2).getText(), "SqFt");
+        Assert.assertEquals(ob5.detailbd.get(3).getText(), "Year Built");
+        Assert.assertEquals(ob5.detailsqft.get(0).isDisplayed() && ob5.detailsqft.get(1).isDisplayed() && ob5.detailsqft.get(2).isDisplayed() && ob5.detailsqft.get(3).isDisplayed(), true);
+		Assert.assertEquals(ob5.reqinfoname.isDisplayed() && ob5.reqinfophone.isDisplayed() && ob5.reqinfoemail.isDisplayed() && ob5.reqinfomessage.isDisplayed() && ob5.reqinfosubmit.isDisplayed(), true);
+        Assert.assertEquals(ob5.reqinfomessage.getText(), "I am interested in speaking with you about the property you have listed.");
+		Assert.assertEquals(ob5.reqinfosubmit.getText(), "REQUEST INFO");
+		Assert.assertEquals(ob5.similarlabel.getText(), "Similar Properties");
+		Thread.sleep(2000);
+	}
+	
+	@Test(enabled=true)
+	public void c_images() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		ob6.tile.click();
+		Thread.sleep(3000);
+		try
+		{
+			Assert.assertEquals(ob5.images.isDisplayed() && ob5.images1.size() > 0, true);
+			System.out.println("Number of Images: "+ob5.images1.size());
+		}
+		catch(Exception e)
+		{
+			Assert.assertEquals(false, false);
+		}
 	}
 	
 	@AfterMethod
